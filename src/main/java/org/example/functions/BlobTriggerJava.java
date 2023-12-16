920ebaa1cada301e3c374dcb972ba442f4012700
MurthyGitHub5 <92693883+MurthyGitHub5@users.noreply.github.com>
2023-12-16 01:37:24 -0500

    Adding BlobTrigger Initial

---
+++ src/main/java/org/example/functions/BlobTriggerJava.java
@@ -0,0 +1,25 @@
+package org.example.functions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.BlobTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.StorageAccount;
+
+/**
+ * Azure Functions with Azure Blob trigger.
+ */
+public class BlobTriggerJava {
+    /**
+     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
+     */
+    @FunctionName("BlobTriggerJava")
+    @StorageAccount(value = "AzureWebJobsStorage")
+    public void run(
+        @BlobTrigger(name = "content", path = "testblobtrigger/{name}", dataType = "binary") byte[] content,
+        @BindingName("name") String name,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Blob trigger function processed a blob. Name: " + name + "\n  Size: " + content.length + " Bytes");
+    }
+}
